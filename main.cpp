fb8aa543f3c5af9460bba1acfccd85448fb7d020
LNG <thisis228228@gmail.com>
2024-10-18 23:20:20 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+int MAIN_FUNC() {
+    while (true) {
+               if (GetAsyncKeyState(VK_NUMPAD9) & 0x8000) {
+            system("powershell -Command \"Disable-NetAdapter -Name 'Ethernet' -Confirm:$false\"");
+            cout << "Ethernet disabled." << std::endl;
+            Sleep(1000); 
+        }
+        else if (GetAsyncKeyState(VK_NUMPAD8) & 0x8000) {
+            system("powershell -Command \"Enable-NetAdapter -Name 'Ethernet' -Confirm:$false\"");
+            cout << "Ethernet enabled." << std::endl;
+            Sleep(1000);  
+        }
+                else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+            cout << "Exiting program..." << std::endl;
+            return 0;
+        }
+        Sleep(100); 
+    }
+}
+
+int main() {
+    MAIN_FUNC();
+    return 0;
+}
